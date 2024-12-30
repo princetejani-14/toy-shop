e649fc618b0a854a91313be3a7367a75bbb21437
princetejani-14 <princetejani14@gmail.com>
2024-12-30 12:25:51 +0530

    class off

--- src/modules/home/shopby-friends/index.js
+++ src/modules/home/shopby-friends/index.js
@@ -59,6 +59,173 @@ export default function Shopbyfriends() {
                             <del>$20.00</del>
                         </div>
                         <div className='shopby_friends_box1_bottom_cart_button'>
+                            <div className='shopby_friends_box1_bottom_cart_button_left'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <div className='shopby_friends_box1_bottom_cart_button_right'>
+                                <button>Add to cart</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className='shopby_friends_box1'>
+                        <div className='shopby_friends_box1_img'>
+                            <img src={img1} />
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_left'>
+                            <p>-16%</p>
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_right'>
+                            <div className='heart_icone'>
+                                <IoMdHeartEmpty className='heart' />
+                            </div>
+                            <div className='eyes_icone'>
+                                <FaRegEye className='eyes' />
+                            </div>
+                            <div className='revers_icone'>
+                                <BiRefresh className='revers' />
+                            </div>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_star_main'>
+                            <div className='shopby_friends_box1_bottom_star'>
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star2' />
+                            </div>
+                            <div className='shopby_friends_box1_bottom_star_flex'>
+                                <p>(02)</p>
+                            </div>
+
+
+                        </div>
+                        <div className='shopby_friends_box1_bottom_data'>
+                            <h1>Huggable Friends</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur. Id viverra cursus enim risus mattis urnanullam</p>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_prize'>
+                            <p>$18.00</p>
+                            <del>$20.00</del>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_cart_button'>
+                            <div className='shopby_friends_box1_bottom_cart_button_left'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <div className='shopby_friends_box1_bottom_cart_button_right'>
+                                <button>Add to cart</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className='shopby_friends_box1'>
+                        <div className='shopby_friends_box1_img'>
+                            <img src={img1} />
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_left'>
+                            <p>-16%</p>
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_right'>
+                            <div className='heart_icone'>
+                                <IoMdHeartEmpty className='heart' />
+                            </div>
+                            <div className='eyes_icone'>
+                                <FaRegEye className='eyes' />
+                            </div>
+                            <div className='revers_icone'>
+                                <BiRefresh className='revers' />
+                            </div>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_star_main'>
+                            <div className='shopby_friends_box1_bottom_star'>
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star2' />
+                            </div>
+                            <div className='shopby_friends_box1_bottom_star_flex'>
+                                <p>(02)</p>
+                            </div>
+
+
+                        </div>
+                        <div className='shopby_friends_box1_bottom_data'>
+                            <h1>Huggable Friends</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur. Id viverra cursus enim risus mattis urnanullam</p>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_prize'>
+                            <p>$18.00</p>
+                            <del>$20.00</del>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_cart_button'>
+                            <div className='shopby_friends_box1_bottom_cart_button_left'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <div className='shopby_friends_box1_bottom_cart_button_right'>
+                                <button>Add to cart</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className='shopby_friends_box1'>
+                        <div className='shopby_friends_box1_img'>
+                            <img src={img1} />
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_left'>
+                            <p>-16%</p>
+                        </div>
+                        <div className='shopby_friends_box1_img_upper_right'>
+                            <div className='heart_icone'>
+                                <IoMdHeartEmpty className='heart' />
+                            </div>
+                            <div className='eyes_icone'>
+                                <FaRegEye className='eyes' />
+                            </div>
+                            <div className='revers_icone'>
+                                <BiRefresh className='revers' />
+                            </div>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_star_main'>
+                            <div className='shopby_friends_box1_bottom_star'>
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star1' />
+                                <IoMdStar className='star2' />
+                            </div>
+                            <div className='shopby_friends_box1_bottom_star_flex'>
+                                <p>(02)</p>
+                            </div>
+
+
+                        </div>
+                        <div className='shopby_friends_box1_bottom_data'>
+                            <h1>Huggable Friends</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur. Id viverra cursus enim risus mattis urnanullam</p>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_prize'>
+                            <p>$18.00</p>
+                            <del>$20.00</del>
+                        </div>
+                        <div className='shopby_friends_box1_bottom_cart_button'>
+                            <div className='shopby_friends_box1_bottom_cart_button_left'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <div className='shopby_friends_box1_bottom_cart_button_right'>
+                                <button>Add to cart</button>
+                            </div>
 
                         </div>
 
